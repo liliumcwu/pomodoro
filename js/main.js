e7b7defd455481451c9a801e142b53b33bdfc038
Lilium Wu <lilium.wu@yale.edu>
2017-06-13 11:12:11 -0700

    disappears input during task, fails to write alarm

--- js/main.js
+++ js/main.js
@@ -14,8 +14,29 @@ var currentColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(
 var workTime = 25;
 var breakTime = 5;
 
+var snd = new Audio('sounds/old-fashioned-school-bell-daniel_simon.mp3'); // buffers automatically when created
+console.log('snd is ' + snd);
+
+//audio loop
+snd.addEventListener('ended', function() {
+  this.currentTime = 0;
+  this.play();
+}, false);
+
+//delays audio stop for loop to run
+var stopAudioDelay = function() {
+  audioTimeoutId = window.setTimeout(stopAudioLoop, 10*1000);
+}
+
+// function that stops audio
+function stopAudioLoop() {
+  snd.pause();
+}
+
+
 function startTask() {
   $startButton.style.visibility = 'hidden';
+  $taskInput.style.visibility = 'hidden';
   // currentColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
   numTasks++;
   var str = taskInput.value;
@@ -24,7 +45,8 @@ function startTask() {
   $status.innerHTML = 'Currently working on <span style="color:' + currentColor + '">' + str + '<//span>.';
 
   taskArray.push(str);
-  startWork(60 * workTime, $timeDisplay);
+  // startWork(60 * workTime, $timeDisplay);
+  startWork(workTime, $timeDisplay);
 }
 
 function startWork(duration, display) {
@@ -39,6 +61,24 @@ function startWork(duration, display) {
     display.textContent = minutes + ":" + seconds;
 
     if (--timer < 0) {
+
+      var playPromise = snd.play();
+      console.log('playPromise is ' + playPromise);
+      // In browsers that don’t yet support this functionality,
+      // playPromise won’t be defined.
+      if (playPromise !== undefined) {
+        playPromise.then(function() {
+          console.log('herro');
+          // Automatic playback started!
+          stopAudioDelay();
+        }).catch(function(error) {
+          console.log('playback failed');
+          snd.play();
+          // Automatic playback failed.
+          // Show a UI element to let the user manually start playback.
+        });
+      }
+
       startBreak(display);
       clearInterval(refreshId);
     }
@@ -55,8 +95,10 @@ function startBreak(display) {
   else
     $tasksCompleted.innerHTML += ', <span style="color:' + currentColor + '">' + taskArray[taskArray.length - 1] + '<//span>';
 
-  if (taskArray.length < 3) timer = 60 * breakTime;
-  else timer = 60 * 2 * breakTime; // double break once 3 tasks are completed
+  // if (taskArray.length < 3) timer = 60 * breakTime;
+  // else timer = 60 * 2 * breakTime; // double break once 3 tasks are completed
+  if (taskArray.length < 3) timer = breakTime;
+  else timer = 2 * breakTime;
 
   var refreshId = setInterval(function() {
     minutes = parseInt(timer / 60, 10);
@@ -71,6 +113,7 @@ function startBreak(display) {
       currentColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
       $taskInput.style.color = currentColor;
       $startButton.style.visibility = 'visible';
+      $taskInput.style.visibility = 'visible';
       $status.innerHTML = 'Break\'s over!';
       clearInterval(refreshId);
     }
