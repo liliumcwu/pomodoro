d6f5ec3257fa438da080487843a881061aaac215
Lilium Wu <lilium.wu@yale.edu>
2017-06-15 14:25:33 -0700

    changes seconds to minutes

--- js/main.js
+++ js/main.js
@@ -14,24 +14,24 @@ var currentColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(
 var workTime = 25;
 var breakTime = 5;
 
-var snd = new Audio('sounds/old-fashioned-school-bell-daniel_simon.mp3'); // buffers automatically when created
-console.log('snd is ' + snd);
+// var snd = new Audio('sounds/old-fashioned-school-bell-daniel_simon.mp3'); // buffers automatically when created
+// console.log('snd is ' + snd);
 
 //audio loop
-snd.addEventListener('ended', function() {
-  this.currentTime = 0;
-  this.play();
-}, false);
+// snd.addEventListener('ended', function() {
+//   this.currentTime = 0;
+//   this.play();
+// }, false);
 
-//delays audio stop for loop to run
-var stopAudioDelay = function() {
-  audioTimeoutId = window.setTimeout(stopAudioLoop, 10*1000);
-}
+// //delays audio stop for loop to run
+// var stopAudioDelay = function() {
+//   audioTimeoutId = window.setTimeout(stopAudioLoop, 10*1000);
+// }
 
-// function that stops audio
-function stopAudioLoop() {
-  snd.pause();
-}
+// // function that stops audio
+// function stopAudioLoop() {
+//   snd.pause();
+// }
 
 
 function startTask() {
@@ -45,8 +45,8 @@ function startTask() {
   $status.innerHTML = 'Currently working on <span style="color:' + currentColor + '">' + str + '<//span>.';
 
   taskArray.push(str);
-  // startWork(60 * workTime, $timeDisplay);
-  startWork(workTime, $timeDisplay);
+  startWork(60 * workTime, $timeDisplay);
+  // startWork(workTime, $timeDisplay); // in seconds
 }
 
 function startWork(duration, display) {
@@ -62,22 +62,22 @@ function startWork(duration, display) {
 
     if (--timer < 0) {
 
-      var playPromise = snd.play();
-      console.log('playPromise is ' + playPromise);
-      // In browsers that don’t yet support this functionality,
-      // playPromise won’t be defined.
-      if (playPromise !== undefined) {
-        playPromise.then(function() {
-          console.log('herro');
-          // Automatic playback started!
-          stopAudioDelay();
-        }).catch(function(error) {
-          console.log('playback failed');
-          snd.play();
-          // Automatic playback failed.
-          // Show a UI element to let the user manually start playback.
-        });
-      }
+      // var playPromise = snd.play();
+      // console.log('playPromise is ' + playPromise);
+      // // In browsers that don’t yet support this functionality,
+      // // playPromise won’t be defined.
+      // if (playPromise !== undefined) {
+      //   playPromise.then(function() {
+      //     console.log('herro');
+      //     // Automatic playback started!
+      //     stopAudioDelay();
+      //   }).catch(function(error) {
+      //     console.log('playback failed');
+      //     snd.play();
+      //     // Automatic playback failed.
+      //     // Show a UI element to let the user manually start playback.
+      //   });
+      // }
 
       startBreak(display);
       clearInterval(refreshId);
@@ -95,10 +95,10 @@ function startBreak(display) {
   else
     $tasksCompleted.innerHTML += ', <span style="color:' + currentColor + '">' + taskArray[taskArray.length - 1] + '<//span>';
 
-  // if (taskArray.length < 3) timer = 60 * breakTime;
-  // else timer = 60 * 2 * breakTime; // double break once 3 tasks are completed
-  if (taskArray.length < 3) timer = breakTime;
-  else timer = 2 * breakTime;
+  if (taskArray.length < 3) timer = 60 * breakTime;
+  else timer = 60 * 2 * breakTime; // double break once 3 tasks are completed
+  // if (taskArray.length < 3) timer = breakTime; // in seconds
+  // else timer = 2 * breakTime; // in seconds
 
   var refreshId = setInterval(function() {
     minutes = parseInt(timer / 60, 10);
@@ -142,10 +142,10 @@ function rainbow() {
 }
 
 function changeTimes() {
-  workTime = Number(window.prompt('Enter new work time:', ''));
+  workTime = Number(window.prompt('Enter minutes for new work time:', ''));
   while (!Number.isInteger(workTime) || workTime <= 0)
     workTime = Number(window.prompt('Invalid work time. Please enter an integer greater than 0.', ''));
-  breakTime = Number(window.prompt('Enter new break time:', ''));
+  breakTime = Number(window.prompt('Enter minutes for new break time:', ''));
   while (!Number.isInteger(breakTime) || breakTime <= 0)
     breakTime = Number(window.prompt('Invalid break time. Please enter an integer greater than 0.', ''));
 }
